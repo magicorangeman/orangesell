a1ab3e4bd58c9ddaf4549dbb3f6f76be3882f4f8
magicorangeman <hangetsunokodachi@gmail.com>
2022-05-22 21:11:33 +0300

    batrunner
    batrunner

---
+++ botrun.bat
@@ -0,0 +1,15 @@
+@echo off
+
+call %~dp0telegram\venv\Scripts\activate
+
+cd %~dp0telegram
+
+rem set TOKEN=5341688388:AAGzxLta3XjmeSRSwQTds3QzsHdA1kAW_RA
+
+rem python C:\Users\Admin\Desktop\telegram\pandabot.py
+
+rem python C:\Users\Admin\Desktop\telegram\wikigod.py
+
+python C:\Users\Admin\Desktop\telegram\ytparser.py
+
+pause
